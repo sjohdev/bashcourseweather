db985753eb058884717b3c870022603032aed166
BadBlob <sjohdev@proton.me>
2026-09-09 21:25:20 +0200

    Changed forecast data from today to tomorrow and minor fixes for logfile

--- getwttrdata.sh
+++ getwttrdata.sh
@@ -7,7 +7,7 @@ tempnow=$(grep -m 1 "°" rawrep | grep -oE "m\+?[0-9]+.*°" | grep -oE "[0-9]+.\
 echo "Temp in $city is: $tempnow Celsius"
 
 # Extract forecast for tomorrow: 
-tempfc=$(grep -m 3 "°" rawrep | cut -d "C" -f 2 | grep -oE "m\+?[0-9]+.*°" | grep -oE "[0-9]+.\[" | grep -m 1 -oE "[0-9]+")
+tempfc=$(grep -m 3 "°" rawrep | cut -d "C" -f 2 | grep -oE "m\+?[0-9]+.*°" | tail -n 1 | grep -oE "[0-9]+.\[" | grep -m 1 -oE "[0-9]+")
 echo "Temp forecast for $city is: $tempfc Celsius"
 
 #Assign Country and City to variable TZ
@@ -18,5 +18,5 @@ day=$(TZ='Morocco/Casablanca' date -u +%d)
 month=$(TZ='Morocco/Casablanca' date +%m)
 year=$(TZ='Morocco/Casablanca' date +%Y)
 
-record=$(echo -e "$year\t$month\t$day\t$tempnow\t$tempfc C")
+record=$(echo -e "$year\t$month\t$day\t$tempnow\t\t$tempfc C")
 echo "$record">>rx_poc.log
